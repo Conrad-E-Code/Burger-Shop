908e00a871019816a73c10be22ee9f89db75ebbd
Michelle Too <mjxtcodes@gmail.com>
2023-01-03 14:18:36 -0800

    logout

--- burger-store/config/routes.rb
+++ burger-store/config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get '/hello', to: 'application#hello_world'
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
-  post "/login", to: "sessions#create" 
+  post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 end
