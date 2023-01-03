e00fb684b4c957af66f95888cb9f8203fcbada21
Michelle Too <mjxtcodes@gmail.com>
2023-01-03 11:21:37 -0800

    login works, user persists on refresh

--- burger-store/app/controllers/users_controller.rb
+++ burger-store/app/controllers/users_controller.rb
@@ -1,27 +1,27 @@
 class UsersController < ApplicationController
-    
-    def create 
-        byebug
-        user = User.create username: params[:username], password: params[:password], password_confirmation: params[:password_confirmation]
-        if user.valid?
-           session[:user_id] = user.id
-           render json: user, status: :created
-        else
-            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-       end
-   end
+  def create
+    user = User.create username: params[:username], password: params[:password],
+                       password_confirmation: params[:password_confirmation]
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
-   def show
+  def show
     user = User.find_by id: session[:user_id]
     if user
-        render json: user
+      render json: user
     else
-        render json: {errors: ["Unauthorized"] }, status: :unauthorized
+      render json: { errors: ['Unauthorized'] }, status: :unauthorized
     end
-end
-private
-def user_params
-    params.permit(:username, :password, :password_confirmation)
-end
+  end
 
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
 end
