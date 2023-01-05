55bae5b4aa6f04fdb667e1f507f2f35fe7be80f9
Michelle Too <mjxtcodes@gmail.com>
2023-01-04 19:50:08 -0800

    inventory shows up, clear login form onsubmit

--- burger-store/app/controllers/inventories_controller.rb
+++ burger-store/app/controllers/inventories_controller.rb
@@ -1,8 +1,8 @@
 class InventoriesController < ApplicationController
-    def show 
+  def show
     inventory = Inventory.find_by location_id: params[:id]
     inventory_array = []
-    inventory_array << {"patty_count": inventory.patty_count} << {"bun_count": inventory.bun_count} << {"fries_count": inventory.fries_count} << {"id": inventory.id}
+    inventory_array << { "patty_count": inventory.patty_count } << { "bun_count": inventory.bun_count } << { "fries_count": inventory.fries_count } << { "id": inventory.id }
     render json: inventory, status: :ok
-    end
+  end
 end
